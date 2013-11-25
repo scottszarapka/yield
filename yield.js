1ba70d012fd7739852011adce234644cbc6b6908
Scott Szarapka <scott@szarapka.com>
2013-11-25 03:28:04 -0800

    Starting application concepts & packages
    Started basic outline for the concepts that will make up Yield:
    
    Updated package.json.
    
    Added placeholders for example config, config loader, boot loader.
    
    Signed-off-by: Scott Szarapka <scott@szarapka.com>

---
+++ yield.js
@@ -0,0 +1,15 @@
+/**
+ *  Yield
+ *  Boot Loader
+ *
+ *  @copyright  Copyright (C) 2013 Scott Szarapka
+ *  @author  Scott Szarapka [scott@szarapka.com] (www.szarapka.com)
+ *  @license GPL v2 (http://www.gnu.org/licenses/gpl-2.0.txt)
+ *  @package  yield
+ *  @version  0.0.1
+ *
+ */
+
+
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
