56929c121dc8e47fe30d8be89b8e3173fa6e623c
Scott Szarapka <scott@szarapka.com>
2014-01-18 00:48:12 -0800

    Strip Versions from Files
    Stupid idea to add version # to every single comment block.
    
    Signed-off-by: Scott Szarapka <scott@szarapka.com>

--- yield/errors/handler.js
+++ yield/errors/handler.js
@@ -6,7 +6,6 @@
  *  @author  Scott Szarapka [scott@szarapka.com] (www.szarapka.com)
  *  @license GPL v2 (http://www.gnu.org/licenses/gpl-2.0.txt)
  *  @package  yield
- *  @version  0.0.1
  *
  */
 
