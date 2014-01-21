cbfe6b28edcc580f6ab5748f82593a134726da4c
Scott Szarapka <scott@szarapka.com>
2014-01-21 13:44:18 -0800

    Added route for POSTs to /upload
    Again not sure if I want this in the frontend routes, will leave for now.
    
    Signed-off-by: Scott Szarapka <scott@szarapka.com>

--- yield/routes/frontend.js
+++ yield/routes/frontend.js
@@ -1,6 +1,9 @@
-var frontend = require('../controllers/frontend');
+var frontend      = require('../controllers/frontend');
+var images        = require('../controllers/images');
+var middleware    = require('../middleware').middleware;
 
 module.exports = function (server) {
   server.get('/', frontend.home);
   server.get('/p/:page', frontend.pages);
+  server.post('/upload', middleware.busboy, images.upload);
 };
